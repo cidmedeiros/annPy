786139ef1951d743bc7f57400f4435400c7e9e40
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2021-11-03 19:40:46 -0300

    add single training function

--- plotGenerator.py
+++ plotGenerator.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot2d(data):
-    fig, ax = plt.subplots(1,1, figsize=(18,6))
+def plot2d(data,width,height,legendSize='xx-small'):
+    fig, ax = plt.subplots(1,1, figsize=(width,height))
     ax = sns.scatterplot(data=data[data.clf == 1], x='d1', y='d2', color = 'darkblue')
     ax = sns.scatterplot(data=data[data.clf == -1], x='d1', y= 'd2', color='darkgreen')
     ax = sns.lineplot(x=[0,100], y=[0,100], color='purple', legend=False)
     fig.suptitle('Linearly Separable Data', fontsize='large')
-    fig.legend(['Boundary','Class 1','Class 2'], loc='center right', fontsize = 'small', title='Classes')
+    fig.legend(['Boundary','Class 1','Class 2'], loc='center right', fontsize = legendSize, title='Classes')
     plt.show()
--- training.py
+++ training.py
@@ -1,3 +1,13 @@
+def singleTrain(inputs, p, target):
+    """ 
+    inputs: [] -> data with only the dimensions to be passed on to the perceptron;
+    p: Perceptron object -> perceptron to have its weights updated;
+    target: any -> value to compare and calculate error;
+    returns: Perceptron object -> the updated perceptron.
+    """
+    p.train(inputs, target)
+    return p
+
 def roundTrain(df,p,columns,target='clf'):
     """
     data: pandas data frame -> data with only the dimensions to be passed on to the perceptron;
@@ -9,7 +19,10 @@ def roundTrain(df,p,columns,target='clf'):
         inputs = []
         for col in columns:
             inputs.append(row[col])
-        p.train(inputs, row[target])
+
+        p = singleTrain(inputs, p, row[target])
     
     print(f'Perceptron after training -> ${p}')
     return p
+
+
